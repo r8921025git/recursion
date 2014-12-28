4f5ec8868dffdefd2e9fe5f9ad9c6007aa84e8e9
r8921025git <r8921025+git@yahoo.com.tw>
2014-12-27 23:26:59 -0500

    my own method, NOT working correctly

--- powerset.cpp
+++ powerset.cpp
@@ -14,39 +14,39 @@ using std::random_device;
 using std::uniform_int_distribution;
 using std::vector;
 
-void GeneratePowerSetHelper(const vector<int>& S, int m, vector<int>* subset);
-void GeneratePowerSetHelper2(const vector<int>& S, int m, vector<int>* subset);
 
-static int count = 0;
+int count = 0;
 
-// @include
-vector<vector<int>> GeneratePowerSet(const vector<int>& S) {
-    // remove last elem
-    vector<int> subset = S;
-    if (subset.empty()) {
-        vector<vector<int>> nset;
-        return nset;
-    }
-    int b = subset.back();
-    subset.pop_back();
-    // recurse
-    vector<vector<int>> pset = GeneratePowerSet(subset);
-    // append
-    vector<vector<int>> nset = pset;
-    if (pset.empty()) {
-        vector<int> tmp;
-        nset.push_back(tmp);
-        vector<int> tmp2 = {b};
-        nset.push_back(tmp2);
-    }
+
+void Func(vector<int> S, vector<int> cur, vector<vector<int>> &result) {
+    result.push_back(cur);
     
-    for (auto i : pset)      {
-        vector<int> tmp = i;
-        tmp.push_back(b);
-        nset.push_back(tmp);
-    }
-    return nset;
+    // Either include the last item, Or exclude the last item
+    
+    
+    const int N = S.size();
+    if (N==0)
+        return;
+        
+    int last_item = S[N-1];
+    S.pop_back();
+
+    // exclude the last item
+    Func(S, cur, result);
+    // include the last item
+    cur.push_back(last_item);
+    Func(S, cur, result);
+    
 }
+
+
+vector<vector<int>> GeneratePowerSet(vector<int> S) {
+    vector<int> cur;
+    vector<vector<int>> result;
+    Func(S, cur, result);
+    return result;
+}
+
 
 void PrintIt(const vector<int> & subset) {
   count++;
@@ -61,43 +61,18 @@ void PrintIt(const vector<int> & subset) {
 }
 
 
-// 2014/12/2, my version
-void GeneratePowerSetHelper2(const vector<int>& S, int m,
-                            vector<int>* subset) {
-
-}
-
-// original code
-void GeneratePowerSetHelper(const vector<int>& S, int m,
-                            vector<int>* subset) {
-
-  for (int i = m; i < S.size(); ++i) {
-    //cout <<"-push_back\n";
-    subset->emplace_back(S[i]);
-    //cout << "-func()\n";
-    PrintIt(*subset);
-    GeneratePowerSetHelper(S, i + 1, subset);
-    //cout<< "-pop\n";
-    subset->pop_back();
-  }
-}
-// @exclude
 
 int main(int argc, char* argv[]) {
   vector<int> S;
-  if (argc >= 2) {
-    for (int i = 1; i < argc; ++i) {
-      S.emplace_back(atoi(argv[i]));
-    }
-  } else {
+
     default_random_engine gen((random_device())());
     uniform_int_distribution<int> dis(1, 10);
     //S.resize(dis(gen));
-    S.resize(3);
+    S.resize(1);
     for (int i = 0; i < S.size(); ++i) {
       S[i] = i;
     }
-  }
+
   vector<vector<int>> pset = GeneratePowerSet(S);
   for (auto i : pset)      {
       PrintIt(i);
